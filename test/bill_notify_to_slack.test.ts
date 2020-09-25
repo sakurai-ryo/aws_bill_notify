7333aab77f02c2f38ddcf8178ac97e0eaa3c0afb
sakurai-ryo <sakurairyo.10do@gmail.com>
2020-09-25 18:24:21 +0900

    tmp

--- test/bill_notify_to_slack.test.ts
+++ test/bill_notify_to_slack.test.ts
@@ -1,13 +1,25 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as BillNotifyToSlack from '../lib/bill_notify_to_slack-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as BillNotifyToSlack from "../lib/bill_notify_to_slack-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new BillNotifyToSlack.BillNotifyToSlackStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new BillNotifyToSlack.BillNotifyToSlackStack(
+    app,
+    "MyTestStack"
+  );
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
